a0d143dee82270a48ceff06277084816b48a1c18
Fernando M. Pinheiro <fernando.pinheiro@gmail.com>
2013-04-09 14:22:46 -0300

    Informando encoding no leitor, conforme avisado pelo FindBugs.

--- src/main/java/com/devmonsters/cnab/arquivoretorno/Leitor.java
+++ src/main/java/com/devmonsters/cnab/arquivoretorno/Leitor.java
@@ -1,11 +1,5 @@
 package com.devmonsters.cnab.arquivoretorno;
 
-import com.devmonsters.cnab.Banco;
-import com.devmonsters.cnab.CnabFactory;
-import com.devmonsters.cnab.arquivoretorno.header.Header;
-import com.devmonsters.cnab.arquivoretorno.registro.Registro;
-import com.devmonsters.cnab.arquivoretorno.trailer.Trailer;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,13 +7,21 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.Charsets;
+
+import com.devmonsters.cnab.Banco;
+import com.devmonsters.cnab.CnabFactory;
+import com.devmonsters.cnab.arquivoretorno.header.Header;
+import com.devmonsters.cnab.arquivoretorno.registro.Registro;
+import com.devmonsters.cnab.arquivoretorno.trailer.Trailer;
+
 public class Leitor {
     private Header header;
     private Trailer trailer;
     private final List<Registro> registros = new ArrayList<>();
 
     public Leitor(final InputStream is, final Banco banco) throws IOException {
-        try (InputStreamReader isr = new InputStreamReader(is); BufferedReader br = new BufferedReader(isr)) {
+        try (InputStreamReader isr = new InputStreamReader(is, Charsets.UTF_8); BufferedReader br = new BufferedReader(isr)) {
             String linha = null;
             while ((linha = br.readLine()) != null) {
                 if (linha.startsWith("0")) {
